3635e6b7c1b03fb8d900e279991c7719ec49087b
thanhnguyenviet25121994 <thanhnguyenviet25121994@gmail.com>
2024-10-05 23:26:11 +0700

    Add files via upload

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Navigate to the application directory
-cd /home/ubuntu/myflaskapp
-
-# Install dependencies
-pip3 install -r requirements.txt
-
-# Start the Flask app
-nohup python3 app.py &
+pip3 install --break-system-packages Flask
+python3 /home/ubuntu/myflaskapp/app.py

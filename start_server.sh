c7b0a6b6fbdb99d1fa3da555cea193e6c3458047
thanhnguyenviet25121994 <thanhnguyenviet25121994@gmail.com>
2024-10-20 00:22:20 +0700

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo hello
 mkdir -p "/home/ubuntu/test-ok111"
-touch /home/ubuntu/test-ok/test
+touch "/home/ubuntu/test-ok111/test"
 #apt install python3-pip -y
 #pip3 install --break-system-packages Flask
 #python3 app.py

e1fac7ff293f0bb1e7d62bfea1f66ed4449f290f
Your Name <you@example.com>
2024-10-20 00:40:08 +0700

    123

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #/bin/sh!
 echo hello
-mkdir /home/ubuntu/test-vs-cdocode
-touch /home/ubuntu/test-vs-cdocode/test-file
+#mkdir /home/ubuntu/test-vs-cdocode
+touch /home/ubuntu/test-file
 docker run -d nginx
